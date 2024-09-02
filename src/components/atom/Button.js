d12996b6c434e9ea43dbe8045c694e0c4688def7
NEX-DoT <nex.dot2@gmail.com>
2024-09-02 22:41:40 +0330

    needed atom components were created

---
+++ src/components/atom/Button.js
@@ -0,0 +1,10 @@
+
+const Button = ({text}) => {
+    return (
+        <button className='h-12 bg-red-600 w-44 rounded-full font-extrabold text-white'>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/utils/StaticData.js
@@ -0,0 +1,53 @@
+//icons for online pos
+import { FaRegClock } from "react-icons/fa";
+import { IoStatsChart } from "react-icons/io5";
+import { GiSlicedBread } from "react-icons/gi";
+
+export const SoftwareText = [
+    'Work on any device, anywhere you are, with cloud-based tools',
+    'Access your reports and data from any locatio',
+    'Monitor inventory levels from anywhere, and manage stock between locations as needed'
+]
+
+export const PaymentsText = [
+    'Accept popular payment methods, like credit, debit, cash and contactless mobile payments like Apple Pay and Google Pay.',
+    'Secure your transactions with built-in fraud detection, end-to-end encryption and PCI compliant hardware.',
+    'Keep more of your hard-earned money with an affordable rate starting at 2.6% + $0.10.'
+]
+const  pic1 = require('../image/businesses/1.png');
+const  pic2 = require('../image/businesses/2.png');
+const  pic3 = require('../image/businesses/3.png');
+const  pic4 = require('../image/businesses/4.png');
+const  pic5 = require('../image/businesses/5.png');
+const  pic6 = require('../image/businesses/6.png');
+const  pic7 = require('../image/businesses/7.png');
+const  pic8 = require('../image/businesses/8.png');
+const  pic9 = require('../image/businesses/9.png');
+
+export const pics = [
+    pic1,
+    pic2,
+    pic3,
+    pic4,
+    pic5,
+    pic6,
+    pic7,
+    pic8,
+    pic9,
+]
+
+export const DataPosOnline = [
+    {
+        icon: <FaRegClock/>,
+        title: 'Get started quickly',
+        description:'Set up your online store for your retail shop quickly with free, easy-to-use professional themes that help build your brand'
+    },{
+        icon: <IoStatsChart/>,
+        title: 'Work efficiently',
+        description:'Seamlessly sync sales and inventory data between your eCom shop and POS system to streamline operations and prevent out of stock issues'
+    },{
+        icon: <GiSlicedBread/>,
+        title: 'Connect your restaurant',
+        description:'Give diners a way to browse your menu ahead of time and offer online ordering for pickup or delivery'
+    },
+]
