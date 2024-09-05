d20d00317823eec057b9870fbbf5ba0d8c656863
NEX-DoT <nex.dot2@gmail.com>
2024-09-05 07:32:39 +0330

    footer and menu update

--- src/components/orgonism/menu.js
+++ src/components/orgonism/menu.js
@@ -22,7 +22,10 @@ const Menu = () => {
 /* -------------------------------- handelers ------------------------------- */
     const onMouseHandeler = (name)=>{
         setOpen({
-            ...open ,
+            business:false,
+            product:false,
+            partners:false,
+            resources:false,
             [name] : true
         })
         console.log(open)
@@ -58,13 +61,13 @@ const Menu = () => {
                 <div className='flex items-center gap-5 md:hidden text-lg'>
                     <FaPhoneAlt/>
                     <GiHamburgerMenu onClick={()=>setHamburger(!hamburger)}/>
-                    <div className={` absolute transition-all duration-500 h-20 -top-4 -right-0 ${!hamburger && '-right-[500px]'} bg-red-500 z-50 w-full text-white flex text-4xl`}>
+                    <div className={` absolute transition-all duration-500 h-20 -top-4 -right-0 ${!hamburger && '-right-[850px]'} bg-red-500 z-50 w-full text-white flex text-4xl`}>
                         <span className='w-full flex items-center justify-between p-3'>
                             <img src={hamburgerLogo} alt="hamburger" className=' w-20 filter invert mix-blend-lighten' />
                             <IoMdClose onClick={()=>setHamburger(!hamburger)}/>
                         </span>
                     </div>
-                    <div className={`absolute transition-all duration-700 -top-4 right-0 ${ !hamburger && '-right-[500px]'} w-10/12 bg-2 shadow-2xl h-svh py-24 px-6`}>
+                    <div className={`absolute transition-all duration-700 -top-4 right-0 ${ !hamburger && '!-right-[700px]'} w-10/12 bg-2 shadow-2xl h-svh py-24 px-6`}>
                         <span className='flex flex-col items-center gap-3'>
                             <button className=' w-full h-16 border font-normal rounded-sm hover:bg-1'>menu item</button>
                             <button className=' w-full h-16 border font-normal rounded-sm hover:bg-1'>menu item2</button>
@@ -75,8 +78,8 @@ const Menu = () => {
 {/* -------------------------------- list menu ------------------------------- */}
                 {open.business  &&   <div className=' absolute top-14 shadow-lg border left-[180px] rounded-md bg-2 w-[730px] h-[470px]' onMouseEnter={()=>onMouseHandeler('business')} onMouseLeave={()=>offMouseHandeler('business')} ><Product/></div>}
                 {open.product   &&   <div className=' absolute top-14 shadow-lg border left-[325px] rounded-md bg-2 w-[730px] h-[470px]' onMouseEnter={()=>onMouseHandeler('product')} onMouseLeave={()=>offMouseHandeler('product')} ><Product/></div>}
-                {open.partners  &&   <div className=' absolute top-14 shadow-lg border left-[435px] rounded-md bg-2 w-[250px] h-[160px]' onMouseEnter={()=>onMouseHandeler('partners')} onMouseLeave={()=>offMouseHandeler('partners')} ><Partners/></div>}
-                {open.resources &&   <div className=' absolute top-14 shadow-lg border left-[910px] rounded-md bg-2 w-[170px] h-[170px]' onMouseEnter={()=>onMouseHandeler('resources')} onMouseLeave={()=>offMouseHandeler('resources')} ><Resources/></div>}
+                {open.partners  &&   <div className=' absolute top-14 shadow-lg border left-[405px] rounded-md bg-2 w-[250px] h-[160px]' onMouseEnter={()=>onMouseHandeler('partners')} onMouseLeave={()=>offMouseHandeler('partners')} ><Partners/></div>}
+                {open.resources &&   <div className=' absolute top-14 shadow-lg border left-[890px] rounded-md bg-2 w-[170px] h-[170px]' onMouseEnter={()=>onMouseHandeler('resources')} onMouseLeave={()=>offMouseHandeler('resources')} ><Resources/></div>}
             </section>
         </menu>
     );
