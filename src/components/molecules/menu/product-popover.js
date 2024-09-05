00acf4cb24328ab66849379cd047f72bb79a111d
NEX-DoT <nex.dot2@gmail.com>
2024-09-05 08:42:02 +0330

    updating product popover

--- src/components/molecules/menu/product-popover.js
+++ src/components/molecules/menu/product-popover.js
@@ -1,36 +1,18 @@
+import { productListData } from "../../../utils/menu-list-data";
 const Product = () => {
   return (
     <div className=" flex items-start justify-around w-full h-full p-10 className='text-base'">
       <ul className="flex flex-col items-start h-full gap-5 relative before:absolute before:h-full before:w-[1px] before:bg-zinc-200 before:-right-10 ">
         <h3 className="text-base">Retail</h3>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Point of Sale</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Payments</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">eCommerce</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Inventory Management</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Inventory Managemen</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Advanced Marketin</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Lightspeed Insight</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Scanne</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Hardwar</li>
+        {productListData[0].map((item , index)=>(<li className="font-semibold hover:text-orange-500 cursor-pointer" key={index}>{item}</li>))}
       </ul>
       <ul className="flex flex-col items-start h-full gap-5 relative before:absolute before:h-full before:w-[1px] before:bg-zinc-200 before:-right-10">
         <h3 className="text-base">Restaurant</h3>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Point of Sale</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Order Anywhere</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Advanced Insights</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Inventory</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Tableside</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Payments</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Accounting</li>
+        {productListData[1].map((item , index)=>(<li className="font-semibold hover:text-orange-500 cursor-pointer" key={index}>{item}</li>))}
       </ul>
       <ul className="flex flex-col items-start gap-5">
         <h3 className="text-base">Golf</h3>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Tee Sheet</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Point of Sale</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Sales & Marketing</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Reporting & Intelligence</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Website & App</li>
-        <li className="font-semibold hover:text-orange-500 cursor-pointer">Accounting</li>
+        {productListData[2].map((item , index)=>(<li className="font-semibold hover:text-orange-500 cursor-pointer" key={index}>{item}</li>))}
       </ul>
     </div>
   );
